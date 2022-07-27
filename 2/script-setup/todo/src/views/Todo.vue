111386f1462e7a635e7f86d05c54dc460688a880
Latrevion <xiaopikaqiu@outlook.com>
2022-07-27 22:09:27 +0800

    suspense处理全局异步

---
+++ 2/script-setup/todo/src/views/Todo.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {ref} from "vue"
+const todos = ref([])
+
+todos.value =await fetch('http://127.0.0.1:3003/news').then(r=>{
+    return r.json()
+    // return new Promise(resolve => {
+    //   setTimeout(()=>resolve(r.json()),2000)
+    // })
+})
+
+
+
+</script>
+
+<template>
+  <div>
+    {{ todos }}
+  </div>
+</template>
+
+<style scoped>
+div{
+  color: #ffffff;
+}
+</style>
