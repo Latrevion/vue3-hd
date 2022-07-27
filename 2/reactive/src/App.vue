3b613167c96eb7fd3807298b3ec55833d79e6ba8
Latrevion <xiaopikaqiu@outlook.com>
2022-07-27 16:00:44 +0800

    mock

--- 2/reactive/src/App.vue
+++ 2/reactive/src/App.vue
@@ -1,31 +1,21 @@
 <template>
   <div>
     <button @click="change">
-      {{ hd.name }}
+      {{name }}
     </button>
   </div>
 </template>
 
 <script>
-import {ref, reactive} from "vue"
-
+import {ref, reactive, toRefs} from "vue"
+//toRefs批量处理为响应数据
 export default {
   setup() {
-    //基本数据类型就选ref 然后加上value修改
-    //对象数组可以使用reactive或ref，变成响应式数据
 
-    //使用ref让基本类型变成引用类型
-    // const hd = ref(9)
-    // const hd = ref([])
-    // const hd = reactive([])
     const hd = reactive({name: "houdun"})
 
-    // const change=()=>hd.value=100
-
-    //本身有引用特性的，使用reactive 可以省略value
-    // const change=()=>hd.push(100)
     const change = () => hd.name = "uncle"
-    return {hd, change}
+    return {...toRefs(hd), change}
   }
 }
 </script>
