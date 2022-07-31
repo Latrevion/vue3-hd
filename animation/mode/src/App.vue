94daba8e850c6f877aa7d4b4886dab5c313b3a4c
Latrevion <xiaopikaqiu@outlook.com>
2022-07-31 22:00:58 +0800

    多标签的动画过渡

--- animation/mode/src/App.vue
+++ animation/mode/src/App.vue
@@ -1,24 +1,47 @@
 <script setup>
+import "animate.css"
 import {ref} from "vue"
 
 const action = ref("on")
+
 </script>
 
 <template>
-  <button v-if="action==='on'" @click="action='off'" class="on">开启</button>
-  <button v-else @click="action='on'" class="off">关闭</button>
+  <section>
+    <transition
+        enter-active-class="animate__animated animate__zoomIn"
+        leave-active-class="animate__animated animate__zoomOut"
+        mode="out-in"
+    >
+      <button v-if="action==='on'" @click="action='off'" class="on">开启</button>
+      <button v-else @click="action='on'" class="off">关闭</button>
+    </transition>
+  </section>
 </template>
 
-<style lang="scss"  scoped>
-button{
+<style lang="scss">
+//section{
+//  position: relative;
+//  button{
+//    position: absolute;
+//  }
+//}
+.animate__animated{
+  animation-duration: 10s;
+}
+
+
+button {
   border: none;
-  padding: 5px 10px ;
+  padding: 5px 10px;
   color: #ffffff;
   cursor: pointer;
-  &.on{
+
+  &.on {
     background: #16a085;
   }
-  &.off{
+
+  &.off {
     background: #d35400;
   }
 }
