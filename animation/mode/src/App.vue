7851864ce38e0ec434c11d129964fadd28ab3b31
Latrevion <xiaopikaqiu@outlook.com>
2022-07-31 21:10:13 +0800

    创建多元素切换项目

---
+++ animation/mode/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import {ref} from "vue"
+
+const action = ref("on")
+</script>
+
+<template>
+  <button v-if="action==='on'" @click="action='off'" class="on">开启</button>
+  <button v-else @click="action='on'" class="off">关闭</button>
+</template>
+
+<style lang="scss"  scoped>
+button{
+  border: none;
+  padding: 5px 10px ;
+  color: #ffffff;
+  cursor: pointer;
+  &.on{
+    background: #16a085;
+  }
+  &.off{
+    background: #d35400;
+  }
+}
+</style>
