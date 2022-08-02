0768cb27d052ff7c92cd146036dea9e1b4ef9bd7
Latrevion <xiaopikaqiu@outlook.com>
2022-08-02 15:29:43 +0800

    TS自动推断

---
+++ vue-ts/vue-ts/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HD from './components/hd.vue'
+
+</script>
+
+<template>
+  <suspense>
+    <HD></HD>
+  </suspense>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-ts/vue-ts/src/components/hd.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {ref} from "vue"
+const data = await fetch('http://127.0.0.1:3003/posts/1').then(r =>r.json())
+const post = ref(data)
+
+type user = {age:number,name?:string}
+let f:user|string|boolean= {age:12}
+f = 'bad'
+</script>
+
+<template>
+{{post.title}}
+</template>
+
+<style scoped>
+
+</style>
