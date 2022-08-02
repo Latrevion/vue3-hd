5307d82b861342382bca440cba799e89c4f6be9d
Latrevion <xiaopikaqiu@outlook.com>
2022-08-02 16:02:46 +0800

    异步请求数据类型提示

--- vue-ts/vue-ts/src/components/hd.vue
+++ vue-ts/vue-ts/src/components/hd.vue
@@ -1,17 +1,26 @@
 <script setup lang="ts">
-import {ref} from "vue"
-const data = await fetch('http://127.0.0.1:3003/posts/1').then(r =>r.json())
-const post = ref(data)
+import {ref} from 'vue';
 
-type user = {age:number,name?:string}
-let f:user|string|boolean= {age:12}
-f = 'bad'
+const res = await fetch('http://127.0.0.1:3003/posts').then(r => r.json());
+const article = ref<{ title: string }>(res);
+
+const state = ref(false);
+
+// const data = await fetch('http://127.0.0.1:3003/posts/1').then(r =>r.json())
+// const post = ref(data)
+//
+// type user = {age:number,name?:string}
+// let f:user|string|boolean= {age:12}
+// f = 'bad'
 </script>
 
 <template>
-{{post.title}}
+  <button @click="state =!state">button</button>
+  <div v-if="state">
+    {{ article.title}}
+  </div>
 </template>
 
-<style scoped>
 
+<style scoped>
 </style>
